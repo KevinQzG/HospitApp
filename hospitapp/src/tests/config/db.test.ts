98a1853f1568460f4873b5e4106011cddf0069df
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2025-02-26 10:24:48 -0500

    Unnecessary Library Removed from DB Tests

--- hospitapp/src/tests/config/db.test.ts
+++ hospitapp/src/tests/config/db.test.ts
@@ -1,31 +1,14 @@
 import MongoDB from '@/config/db';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import { ObjectId } from 'mongodb';
 
 describe('MongoDB Connection', () => {
-  let mongoServer: MongoMemoryServer;
   const _FILTER = { _id: new ObjectId('67b7885ec6dcb343450c0582') };
   const _EXPECTED_RESULT = [{ _id: new ObjectId('67b7885ec6dcb343450c0582'), name: "COMFAMILIARCAMACOL EPS-S", "01_8000_phone": "018000 111776", fax: "2609442", emails: "REFERENCIA@COMFAMILIARCAMACOL.COM"}];
 
   // Increase timeout to 30 seconds for all hooks and tests
   jest.setTimeout(30000);
 
-  beforeAll(async () => {
-    try {
-      // Start MongoDB Memory Server
-      mongoServer = await MongoMemoryServer.create();
-      process.env.DATABASE_URL = mongoServer.getUri();
-      process.env.DATABASE_NAME = 'HospitAppDev';
-    } catch (err) {
-      console.error('Failed to start MongoDB Memory Server:', err);
-      throw err;
-    }
-  });
-
   afterAll(async () => {
-    if (mongoServer) {
-      await mongoServer.stop();
-    }
     await MongoDB.close();
   });
 
