ed5f3e997b394a152843f8a8c540614918dcbb36
julimejia <davidmejiao43@gmail.com>
2025-03-13 23:35:04 -0500

    slint

--- hospitapp/.eslint.config.js
+++ hospitapp/.eslint.config.js
@@ -1,38 +1,46 @@
-module.exports = {
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-  plugins: ["@typescript-eslint", "jsdoc"],
-  rules: {
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        selector: "variable",
-        format: ["snake_case"],
-        filter: {
-          regex: "^(?!.*\\(\\)).*$",
-          match: true
+export default [
+  {
+    ignores: ["node_modules/", "dist/"], // Ignorar carpetas innecesarias
+  },
+  {
+    extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      jsdoc: require("eslint-plugin-jsdoc"),
+    },
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["snake_case"],
+          filter: {
+            regex: "^(?!.*\\(\\)).*$",
+            match: true
+          }
+        },
+        {
+          selector: "variable",
+          modifiers: ["const"],
+          format: ["UPPER_CASE"],
+          prefix: ["_"],
+          filter: {
+            regex: "^(?!.*\\(\\)).*$",
+            match: true
+          }
+        },
+        {
+          selector: "function",
+          format: ["snake_case"]
         }
-      },
-      {
-        selector: "variable",
-        modifiers: ["const"],
-        format: ["UPPER_CASE"],
-        prefix: ["_"],
-        filter: {
-          regex: "^(?!.*\\(\\)).*$",
-          match: true
+      ],
+      camelcase: "off",
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: ["../components/*"]
         }
-      },
-      {
-        selector: "function",
-        format: ["snake_case"]
-      }
-    ],
-    camelcase: "off",
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: ["../components/*"]
-      }
-    ]
+      ]
+    }
   }
-};
+];
