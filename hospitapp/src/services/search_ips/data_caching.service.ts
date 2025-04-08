86e2646a61f9f8384a17aa62b8d1a3a916877d88
Kevin Quiroz <kquirozg@eafit.edu.co>
2025-04-07 23:07:27 -0500

    Fixed bug

--- hospitapp/src/services/search_ips/data_caching.service.ts
+++ hospitapp/src/services/search_ips/data_caching.service.ts
@@ -1,45 +1,46 @@
-import { unstable_cache as cache } from 'next/cache'
-import DBAdapter from '@/adapters/db.adapter';
+import { unstable_cache as cache } from "next/cache";
 import CONTAINER from "@/adapters/container";
-import SearchIpsServiceAdapter from "@/adapters/search_ips.service.adapter";
+import SpecialtyServiceAdapter from "@/adapters/services/specialty.service.adapter";
+import EpsServiceAdapter from "@/adapters/services/eps.service.adapter";
 import { TYPES } from "@/adapters/types";
-import { SpecialtyResponse } from '@/models/specialty.interface';
-import { EpsResponse } from '@/models/eps.interface';
-import { ENV } from '@/config/env';
+import { SpecialtyResponse } from "@/models/specialty.interface";
+import { EpsResponse } from "@/models/eps.interface";
+import { ENV } from "@/config/env";
 
 export interface SearchFormClientProps {
-    specialties: SpecialtyResponse[];
-    eps: EpsResponse[];
-    is_admin?: boolean;
+	specialties: SpecialtyResponse[];
+	eps: EpsResponse[];
 }
 
 // Cache configuration
-const CACHE_TAG = 'search-config';
+const CACHE_TAG = "search-config";
 
 export const getSearchIpsCachedProps = cache(
-    async (): Promise<SearchFormClientProps> => {
-        try {
-            // Inject the dependencies
-            const DB_HANDLER = CONTAINER.get<DBAdapter>(TYPES.DBAdapter);
-            const SEARCH_IPS_SERVICE = CONTAINER.get<SearchIpsServiceAdapter>(TYPES.SearchIpsServiceAdapter);
+	async (): Promise<SearchFormClientProps> => {
+		try {
+			// Inject the dependencies
+			const EPS_SERVICE = CONTAINER.get<EpsServiceAdapter>(
+				TYPES.EpsServiceAdapter
+			);
+			const SPECIALTY_SERVICE = CONTAINER.get<SpecialtyServiceAdapter>(
+				TYPES.SpecialtyServiceAdapter
+			);
 
-            // Fetch the data
-            const RESULTS = {
-                specialties: await SEARCH_IPS_SERVICE.getAllSpecialties(),
-                eps: await SEARCH_IPS_SERVICE.getAllEps()
-            }
+			// Fetch the data
+			const RESULTS = {
+				specialties: await SPECIALTY_SERVICE.getAllSpecialties(),
+				eps: await EPS_SERVICE.getAllEps(),
+			};
 
-            // Close the database connection and return the results
-            await DB_HANDLER.close();
-            return RESULTS;
-        } catch (error) {
-            if (error instanceof Error) {
-                throw new Error(`Error fetching page props: ${error.message}`);
-            } else {
-                throw new Error('Error fetching page props');
-            }
-        }
-    },
-    [CACHE_TAG],
-    { revalidate: ENV.CACHE_TTL, tags: [CACHE_TAG] }
-)
+			return RESULTS;
+		} catch (error) {
+			if (error instanceof Error) {
+				throw new Error(`Error fetching page props: ${error.message}`);
+			} else {
+				throw new Error("Error fetching page props");
+			}
+		}
+	},
+	[CACHE_TAG],
+	{ revalidate: ENV.CACHE_TTL, tags: [CACHE_TAG] }
+);
