374bb0e94db96b1a4e77dd21ffdaf7fccdeda0a9
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2025-04-25 16:55:38 -0500

    feat(Search IPS): :sparkles: Additional Button Added
    I added a new button to simulate the sorting button

--- hospitapp/src/components/ResultsPagePipe.tsx
+++ hospitapp/src/components/ResultsPagePipe.tsx
@@ -415,6 +415,16 @@ function ResultsDisplay({ specialties, eps }: SearchFormClientProps) {
 		  Resultados de Búsqueda
 		</h1>
 		<div className="flex flex-col sm:flex-row space-y-3 sm:space-y-0 sm:space-x-3 items-start sm:items-center w-full sm:w-auto">
+		<div className="relative w-full sm:w-64">
+			<input
+			  type="text"
+			  value={searchQuery}
+			  onChange={(e) => setSearchQuery(e.target.value)}
+			  placeholder="Filtrar resultados..."
+			  className="w-full pl-8 sm:pl-10 pr-4 py-2 rounded-lg bg-gray-100 dark:bg-gray-800 dark:text-white text-gray-800 placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+			/>
+			<Search className="w-4 h-4 sm:w-5 sm:h-5 absolute left-2 sm:left-3 top-1/2 transform -translate-y-1/2 text-gray-600 dark:text-gray-300" />
+		  </div>
 		  <div className="relative w-full sm:w-64">
 			<input
 			  type="text"
