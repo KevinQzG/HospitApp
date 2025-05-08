ecb48eb69cb4a6222f14dd2b9416251679f56764
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2025-05-08 16:19:19 -0500

    refactor(Linter): :rotating_light: Linter Warnings Removed
    Linter Errors Fixed

--- hospitapp/src/steps/sortIpsSteps.ts
+++ hospitapp/src/steps/sortIpsSteps.ts
@@ -1,5 +1,4 @@
-import { When, Then } from "@cucumber/cucumber";
-import { expect } from "chai";
+import { When } from "@cucumber/cucumber";
 import { By, until } from "selenium-webdriver";
 import { driver } from "./commonSteps.ts";
 
