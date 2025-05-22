c90cff3788992d73fc130fa7e59be165d096e3da
Kevin Quiroz <kquirozg@eafit.edu.co>
2025-05-22 10:32:57 -0500

    💡 (route.ts): add whitespace for code readability ♻️ (ResultsPage.tsx): ensure file ends with a newline for POSIX compliance

--- hospitapp/src/app/api/v1.0.0/ips/filter/pagination/route.ts
+++ hospitapp/src/app/api/v1.0.0/ips/filter/pagination/route.ts
@@ -160,6 +160,7 @@ export async function POST(
 	try {
 		// Parse and validate request body
 		const BODY: SearchRequest = await req.json();
+		
 
 		// Body validation
 		const { success: SUCCESS, error: ERROR } = VALIDATE_REQUEST_BODY(BODY);
